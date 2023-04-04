6ac960b469dde74dad5f2b7a59d9ac1e2a658576
sahil <sahil242bhatiya@gmail.com>
2023-04-04 21:19:43 +0530

    minor fix in verification to add todo

--- src/utils/todo-verification.js
+++ src/utils/todo-verification.js
@@ -1,3 +1,3 @@
 export const verifyTodoObject = (todo) => {
-    return !((typeof todo.title !== 'string' && todo.title.length === 0) || (typeof todo.description !== 'string' && todo.description.length === 0));
+    return !(todo.title.trim().length === 0 || todo.description.trim().length === 0);
 }
